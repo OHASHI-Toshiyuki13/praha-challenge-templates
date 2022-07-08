ea6bd0b7f97ea14c2304a15741d55b72601895e9
OHASHI Toshiyuki <108501523+OHASHI-Toshiyuki13@users.noreply.github.com>
2022-07-08 11:10:09 +0900

    単体テスト現状

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,11 @@
 // todo: ここに単体テストを書いてみましょう！
+import * as functions from "../functions";
+
+test('sumOfArray', () => {
+    expect(functions.sumOfArray([1,2,3,4])).toBe(10);
+});
+
+//これは失敗するテスト
+test('asyncSumOfArray',() => {
+    expect(functions.asyncSumOfArray([1])).toBe(1);
+});
